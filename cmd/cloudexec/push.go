183a32f130e2a9efb27b1f5f1f47c8eaf08c46e8
bohendo <bo.henderson@trailofbits.com>
2023-09-27 15:21:55 -0400

    zip target folder before uploading

--- cmd/cloudexec/push.go
+++ cmd/cloudexec/push.go
@@ -1,40 +1,79 @@
 package main
 
 import (
+	"io"
 	"fmt"
 	"os"
 	"path/filepath"
+  "archive/zip"
 
 	"github.com/crytic/cloudexec/pkg/config"
 	"github.com/crytic/cloudexec/pkg/s3"
 )
 
 func UploadDirectoryToSpaces(config config.Config, bucketName string, sourcePath string, destPath string) error {
-	// Walk the directory and upload files recursively
-	return filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
+  // Create a file to store the compressed archive of the input sourcePath
+  zipFilePath := filepath.Join(filepath.Dir(sourcePath), filepath.Base(sourcePath) + ".zip")
+  zipFile, err := os.Create(zipFilePath)
+  if err != nil {
+      return err
+  }
+  defer zipFile.Close()
+
+  archive := zip.NewWriter(zipFile)
+  defer archive.Close()
+
+	// Walk the directory and recursively add files to the zip archive
+  err = filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+    fmt.Printf("Adding %s to the zipped archive", path)
 
-		if !info.IsDir() {
-			// Read the file
-			fileBytes, err := os.ReadFile(path)
-			if err != nil {
-				return fmt.Errorf("Failed to read file %s: %w", path, err)
-			}
+    // Create a new ZIP file entry
+    zipFileEntry, err := archive.Create(path[len(sourcePath):])
+    if err != nil {
+        return err
+    }
 
-			// Compute the destination key (path) in the bucket
-			relativePath, _ := filepath.Rel(sourcePath, path)
-			destinationKey := filepath.Join(destPath, "input", relativePath)
+    // If this is a subdirectory, we're done. Move on.
+		if info.IsDir() {
+      return nil
+    }
 
-			err = s3.PutObject(config, bucketName, destinationKey, fileBytes)
-			if err != nil {
-				return err
-			}
+    file, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
 
-			fmt.Printf("Successfully uploaded %s to %s/%s\n", path, bucketName, destinationKey)
-		}
+    _, err = io.Copy(zipFileEntry, file)
+    if err != nil {
+        return err
+    }
+
 		return nil
 	})
+  if err != nil {
+      return err
+  }
+  fmt.Printf("Successfully added all files from %s to zipped archive at %s\n", sourcePath, zipFilePath)
 
+  // Read the zipped archive
+  fileBytes, err := os.ReadFile(zipFilePath)
+  if err != nil {
+    return fmt.Errorf("Failed to read zipped archive %s: %w", zipFilePath, err)
+  }
+
+  // Compute the destination key in the bucket
+  relativePath, _ := filepath.Rel(sourcePath, zipFilePath)
+  destinationKey := filepath.Join(destPath, "input", relativePath)
+
+  err = s3.PutObject(config, bucketName, destinationKey, fileBytes)
+  if err != nil {
+      return err
+  }
+
+  fmt.Printf("Successfully uploaded %s to %s/%s\n", zipFilePath, bucketName, destinationKey)
+  return nil
 }
