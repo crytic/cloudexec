dda1487566736f630243924c42e9140eca2ef7d1
bohendo <bo.henderson@trailofbits.com>
2024-03-29 11:01:16 -0400

    [packer] dynamically fetch latest release artifacts from github

--- packer/provision.sh
+++ packer/provision.sh
@@ -5,25 +5,19 @@ set -e
 ########################################
 ## Helper Functions
 
-function get_latest_version {
+function github_api {
   project="$1"
   github_api="https://api.github.com/repos/${project}/releases/latest"
-  curl -s "$github_api" | jq '.tag_name' | tr -d 'v' | tr -d '"'
+  curl -s "$github_api"
+}
+
+function get_latest_version {
+  github_api "$1" | jq '.tag_name' | tr -d 'v' | tr -d '"'
 }
 
 function get_latest_artifact {
-  project="$1"
-  github_api="https://api.github.com/repos/${project}/releases/latest"
-  curl -s "$github_api" | jq '.assets[] | select(.name | test("linux")) | .browser_download_url' | grep -v "sigstore" | tr -d '"'
+  github_api "$1" | jq '.assets[] | select(.name | test("linux")) | .browser_download_url' | tr -d '"'
 }
-
-########################################
-## Versions
-
-doctl_version="1.92.0"
-solc_version="0.8.6"
-echidna_version="$(get_latest_version "crytic/echidna")"
-medusa_version="$(get_latest_version "crytic/medusa")"
 
 ########################################
 ## Required Configuration and Dependencies
@@ -38,37 +32,37 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y jq s3cmd tmux python3-pip python3-venv
 
-echo "Downloading doctl..."
-curl -fsSL -o /tmp/doctl-1.92.0-linux-amd64.tar.gz https://github.com/digitalocean/doctl/releases/download/v1.92.0/doctl-1.92.0-linux-amd64.tar.gz
-echo "Extracting doctl..."
-tar -xzf /tmp/doctl-1.92.0-linux-amd64.tar.gz -C /tmp
-echo "Installing doctl..."
+echo "Installing doctl v$(get_latest_version "digitalocean/doctl")..."
+curl -fsSL -o /tmp/doctl.tar.gz "$(get_latest_artifact "digitalocean/doctl" | grep "amd64")"
+tar -xzf /tmp/doctl.tar.gz -C /tmp
 mv /tmp/doctl /usr/local/bin
-echo "Cleaning up..."
-rm /tmp/doctl-1.92.0-linux-amd64.tar.gz
+rm /tmp/doctl.tar.gz
+echo "Done installing: $(doctl version)"
 
 ########################################
 ## Common fuzz testing and analysis tools
 
-echo "Installing slither and solc v${solc_version}..."
+echo "Installing slither..."
 python3 -m venv ~/venv
 source ~/venv/bin/activate
 pip3 install solc-select slither-analyzer crytic-compile
 solc-select use latest --always-install
 
-echo "Installing echidna v${echidna_version}..."
-curl -fsSL -o /tmp/echidna.tar.gz "$(get_latest_artifact "crytic/echidna")"
+echo "Installing echidna v$(get_latest_version "crytic/echidna")..."
+curl -fsSL -o /tmp/echidna.tar.gz "$(get_latest_artifact "crytic/echidna" | grep -v "sigstore")"
 tar -xzf /tmp/echidna.tar.gz -C /tmp
 mv /tmp/echidna /usr/local/bin
 chmod +x /usr/local/bin/echidna
 rm /tmp/echidna.tar.gz
+echo "Done installing: $(echidna --version)"
 
-echo "Installing medusa v${medusa_version}..."
-curl -fsSL -o /tmp/medusa.zip "$(get_latest_artifact "crytic/medusa")"
-unzip /tmp/medusa.zip -d /tmp
+echo "Installing medusa v$(get_latest_version "crytic/medusa")..."
+curl -fsSL -o /tmp/medusa.tar.gz "$(get_latest_artifact "crytic/medusa" | grep -v "sigstore")"
+tar -xzf /tmp/medusa.tar.gz -C /tmp
 sudo mv /tmp/medusa /usr/local/bin
 chmod +x /usr/local/bin/medusa
-rm /tmp/medusa.zip
+rm /tmp/medusa.tar.gz
+echo "Done installing: $(medusa --version)"
 
 echo "Installing docker and its dependencies..."
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -81,3 +75,7 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 user="$(whoami)"
 usermod -aG docker "${user}"
 systemctl enable docker
+echo "Done installing docker:"
+docker version
+
+echo "Done provisioning"
