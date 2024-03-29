35e86164923982785a07df318907b06799256955
bohendo <bo.henderson@trailofbits.com>
2024-03-29 11:56:07 -0400

    trunk fmt

--- packer/provision.sh
+++ packer/provision.sh
@@ -6,17 +6,17 @@ set -e
 ## Helper Functions
 
 function github_api {
-  project="$1"
-  github_api="https://api.github.com/repos/${project}/releases/latest"
-  curl -s "$github_api"
+	project="$1"
+	github_api="https://api.github.com/repos/${project}/releases/latest"
+	curl -s "${github_api}"
 }
 
 function get_latest_version {
-  github_api "$1" | jq '.tag_name' | tr -d 'v' | tr -d '"'
+	github_api "$1" | jq '.tag_name' | tr -d 'v' | tr -d '"'
 }
 
 function get_latest_artifact {
-  github_api "$1" | jq '.assets[] | select(.name | test("linux")) | .browser_download_url' | tr -d '"'
+	github_api "$1" | jq '.assets[] | select(.name | test("linux")) | .browser_download_url' | tr -d '"'
 }
 
 ########################################
@@ -32,8 +32,14 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y jq s3cmd tmux python3-pip python3-venv
 
-echo "Installing doctl v$(get_latest_version "digitalocean/doctl")..."
-curl -fsSL -o /tmp/doctl.tar.gz "$(get_latest_artifact "digitalocean/doctl" | grep "amd64")"
+echo "Installing doctl v$(
+	set -e
+	get_latest_version "digitalocean/doctl"
+)..."
+curl -fsSL -o /tmp/doctl.tar.gz "$(
+	set -e
+	get_latest_artifact "digitalocean/doctl" | grep "amd64"
+)"
 tar -xzf /tmp/doctl.tar.gz -C /tmp
 mv /tmp/doctl /usr/local/bin
 rm /tmp/doctl.tar.gz
@@ -48,16 +54,28 @@ source ~/venv/bin/activate
 pip3 install solc-select slither-analyzer crytic-compile
 solc-select use latest --always-install
 
-echo "Installing echidna v$(get_latest_version "crytic/echidna")..."
-curl -fsSL -o /tmp/echidna.tar.gz "$(get_latest_artifact "crytic/echidna" | grep -v "sigstore")"
+echo "Installing echidna v$(
+	set -e
+	get_latest_version "crytic/echidna"
+)..."
+curl -fsSL -o /tmp/echidna.tar.gz "$(
+	set -e
+	get_latest_artifact "crytic/echidna" | grep -v "sigstore"
+)"
 tar -xzf /tmp/echidna.tar.gz -C /tmp
 mv /tmp/echidna /usr/local/bin
 chmod +x /usr/local/bin/echidna
 rm /tmp/echidna.tar.gz
 echo "Done installing: $(echidna --version)"
 
-echo "Installing medusa v$(get_latest_version "crytic/medusa")..."
-curl -fsSL -o /tmp/medusa.tar.gz "$(get_latest_artifact "crytic/medusa" | grep -v "sigstore")"
+echo "Installing medusa v$(
+	set -e
+	get_latest_version "crytic/medusa"
+)..."
+curl -fsSL -o /tmp/medusa.tar.gz "$(
+	set -e
+	get_latest_artifact "crytic/medusa" | grep -v "sigstore"
+)"
 tar -xzf /tmp/medusa.tar.gz -C /tmp
 sudo mv /tmp/medusa /usr/local/bin
 chmod +x /usr/local/bin/medusa
